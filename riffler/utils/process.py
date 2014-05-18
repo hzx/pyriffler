a7f6a8227ca354d339e5c0ba3d45bc527cb1576f
Dmitry Dementev <dem@oldschooldev.com>
2014-05-18 12:06:21 +0400

    fix work with processes

--- riffler/utils/process.py
+++ riffler/utils/process.py
@@ -4,11 +4,17 @@ import sys
 
 def execute(*command):
   exitcode = subprocess.call(command)
-  if exitcode != 0:
-    sys.exit(exitcode)
+  return exitcode
+  #if exitcode != 0:
+  #  sys.exit(exitcode)
 
 
 # TODO(dem) check CalledProcessError and make sys.exit with code
 def executeWithOutput(*command):
   output = subprocess.check_output(command)
+  return output
+
+
+def executeWithOutputShell(command):
+  output = subprocess.check_output(command, shell=True)
   return output
